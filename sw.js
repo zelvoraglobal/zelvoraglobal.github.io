18be6a9038cf8d4988286b23c5878cbb862c8d5a
zelvoraglobal <zelvora.global@gmail.com>
2026-01-22 11:36:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", () => {});
